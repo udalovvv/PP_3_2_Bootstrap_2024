b67b63c7f888aff952ee69c7bc2c3965cedd3d94
udalovvv <udalov.vitalii@gmail.com>
2024-02-11 16:01:16 +0500

    Add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+Задание:
+1. Изменить страницы вашего приложения в соответствии со скриншотами, используя Bootstrap.
+
+Пользователи:
+1. login: admin@mail.ru  password: admin
+2. login: user@mail.ru  password: user
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 INSERT INTO users (`age`, `email`, `first_name`, `last_name`, `password`)
-VALUES ('1', 'admin@mail.ru', 'admin', 'admin', '$2a$12$cm066YTzIjDk0C361x6u1eKYiBsX0e/3abc5UCm.FvD5xdq4aSwLG');
--- password = 1
+VALUES ('1', 'admin@mail.ru', 'admin', 'admin', '$2a$12$iCZIGBvFU6Yud/n15FBFzu95xo4413RCEr2ou4FVdAxiv1WGmnpXC');
+-- password = admin
 
 INSERT INTO users (`age`, `email`, `first_name`, `last_name`, `password`)
-VALUES ('2', 'user@mail.ru', 'user', 'user', '$2a$12$cm066YTzIjDk0C361x6u1eKYiBsX0e/3abc5UCm.FvD5xdq4aSwLG');
--- password = 1
+VALUES ('2', 'user@mail.ru', 'user', 'user', '$2a$12$1iGRrj7Ho1wyTRvBb73p1.peJUtfJRJOQ5nMLOTjZsIhPMNBuio9C');
+-- password = user
 
 INSERT INTO roles (`role`)
 VALUES ('ADMIN');
